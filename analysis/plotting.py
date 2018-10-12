50999876cbc18a668f6c01ed8ecf7e8151b03c8d
Matt Bierbaum <matt.bierbaum@gmail.com>
2018-10-11 22:48:01 -0400

    consolidate plotting

--- analysis/plotting.py
+++ analysis/plotting.py
@@ -2,11 +2,15 @@ import numpy as np
 import scipy as sp
 import pylab as pl
 import matplotlib
-#matplotlib.rcParams['savefig.dpi'] = 2 * matplotlib.rcParams['savefig.dpi']
+#matplotlib.rcParams['savefig.dpi'] = 300
+matplotlib.rcParams['agg.path.chunksize'] = 10000
+
 from matplotlib.collections import LineCollection
 import yaml
 import os
 import time
+import re
+import glob
 
 def load(base):
     conf = yaml.load(open(base+".conf"))
@@ -108,16 +112,19 @@ def plot_density(base, thin=False, start=0, size=14, save=False):
     pl.tight_layout()
     return track
 
-def plot_file(base, thin=True, start=0, size=10, save=False):
+def plot_track(base, thin=True, start=0, size=10, save=False, showpegs=True, lw=None):
     conf, track, pegs = load(base)
 
-    linethick = 0.0125 if thin else 0.3
+    linethick = lw if lw else 0.0125 if thin else 0.3
 
     fig = pl.figure(figsize=(size,size*conf['top']/conf['wall']))
-    for peg in pegs:
-        pl.gca().add_artist(pl.Circle(peg, conf['radius'], color='k', alpha=0.3))
 
-    pl.plot(track[:,0], track[:,1], '-', linewidth=linethick, alpha=0.8)
+    if showpegs:
+        for peg in pegs:
+            pl.gca().add_artist(pl.Circle(peg, conf['radius'], ec='w', fc='k', alpha=0.0, lw=0.0))
+            pl.gca().add_artist(pl.Circle(peg, conf['radius'], ec='k', fill=False, alpha=0.8, lw=1.0))
+
+    pl.plot(track[:,0], track[:,1], '-', c='k', linewidth=linethick, alpha=0.8)
     pl.xlim(0, conf['wall'])
     pl.ylim(0, conf['top'])
     pl.show()
@@ -127,7 +134,7 @@ def plot_file(base, thin=True, start=0, size=10, save=False):
     if save:
         pl.savefig(base+".png", dpi=200)
 
-def plot_file_color(base, thin=True, start=0, size=14, save=False):
+def plot_track_color(base, thin=True, start=0, size=14, save=False):
     conf, track, pegs = load(base)
 
     fig = pl.figure(figsize=(size,size*conf['top']/conf['wall']))
@@ -158,6 +165,74 @@ def plot_y(base):
     conf, track, pegs = load(base)
     pl.figure(figsize=(6,6))
     pl.plot(track[:,1], lw=1)
+
+
+# ============================================================================
+# plotting functions for densities
+# ============================================================================
+def prefix_twotone(prefix):
+    g0 = glob.glob('{}.*color.npy'.format(prefix))[0]
+    g1 = glob.glob('{}.*count.npy'.format(prefix))[0]
+    return g0, g1
+
+def plot_twotone(prefix, sigma=4, figsize=10):
+    re_size = re.compile(r'{}\.(\d+)-(\d+)_([a-z]+).*'.format(prefix))
+    f0, f1 = prefix_twotone(prefix)
+
+    width, height, dtype = re_size.search(f0).groups()
+    width = int(width)
+    height = int(height)
+    n_color = np.fromfile(f0, dtype=np.dtype(dtype)).reshape(height, width, 3)
+    n_count = np.fromfile(f1, dtype=np.dtype(dtype)).reshape(height, width)
+
+    clip = sigma * n_count[n_count > 0].std()
+    n_count = np.clip(n_count, 0, clip)
+    n_color = 1 - (1 - n_color)*(n_count / n_count.max())[...,None]
+
+    if width > height:
+        height = 10 * height / width
+        width = 10
+    if height >= width:
+        width = 10 * width / height
+        height = 10
+
+    fig = pl.figure(figsize=(width, height))
+    pl.imshow(n_color, origin='lower')
+    pl.xticks([])
+    pl.yticks([])
+    pl.tight_layout()
+
+def plot_indexed_colors(prefix, sigma=4, figsize=10, maps=['Blues', 'gray_r']):
+    re_size = re.compile(r'{}\.(\d+)-(\d+)_([a-z]+).*'.format(prefix))
+    f0 = glob.glob('{}.*count.npy'.format(prefix))[0]
+
+    width, height, dtype = re_size.search(f0).groups()
+    width = int(width)
+    height = int(height)
+    n = np.fromfile(f0, dtype=np.dtype(dtype)).reshape(height, width, -1)
+    n = np.rollaxis(n, 2)
+
+    clip = sigma * n[n > 0].std()
+    n = np.clip(n, 0, clip)
+    #n = 1 - (1 - n)*(n / n.max())
+
+    if width > height:
+        height = 10 * height / width
+        width = 10
+    if height >= width:
+        width = 10 * width / height
+        height = 10
+
+    fig = pl.figure(figsize=(width, height))
+    masked = []
+    for i, t in enumerate(n):
+        tmp = np.ma.masked_array(t, t == 0)
+        pl.imshow(tmp, origin='lower', cmap=maps[i], alpha=1.0/len(n))
+
+    pl.xticks([])
+    pl.yticks([])
+    pl.tight_layout()
+
 
 """
 plot_file('small', True)
