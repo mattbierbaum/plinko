e2d2fd1f26375f5afba72b89170890a4a5dcd26a
Matt Bierbaum <matt.bierbaum@gmail.com>
2018-11-25 22:58:51 -0500

    new scripts

--- lua/scripts/single_circle_orbits.lua
+++ lua/scripts/single_circle_orbits.lua
@@ -12,13 +12,13 @@ local conf = {
     particles = {objects.PointParticle({0.501, 0.85}, {0.3, 0}, {0, 0})},
     objects = {objects.Circle({0.5, 0.5}, 0.45)},
     observers = {
-        observers.StateFileRecorder('./single_circle_orbits.csv'),
+        --observers.StateFileRecorder('./single_circle_orbits.csv'),
         observers.TimePrinter(1e6)
     },
 }
 
 local s = ics.create_simulation(conf)
 local t_start = os.clock()
-s:step(1e7)
+s:step(1e6)
 local t_end = os.clock()
 print(t_end - t_start)
