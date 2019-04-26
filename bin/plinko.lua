5e5f90cf90c0bb17b3528a14888fba2a83c0b026
Matt Bierbaum <matt.bierbaum@gmail.com>
2019-04-25 21:50:30 -0400

    making script loading a library function

--- bin/plinko.lua
+++ bin/plinko.lua
@@ -1,15 +1,7 @@
 local plinko = require('plinko')
 
-_G['P'] = plinko
-_G['plinko'] = plinko
-
-function dofile(filename)
-    local f = assert(loadfile(filename))
-    return f()
-end
-
 if #arg < 1 then
     print("plinko <script.lua> <args>")
 else
-    dofile(table.remove(arg, 1))
+    plinko.run_file(table.remove(arg, 1))
 end
--- plinko/init.lua
+++ plinko/init.lua
@@ -8,4 +8,15 @@ for i, mod in pairs(modules) do
     package[mod] = require('plinko.'..mod)
 end
 
+function package.run_file(filename)
+    local f = assert(loadfile(filename))
+    return f()
+end
+
+function package.run_string(string)
+    local f = assert(loadstring(string))
+    return f()
+end
+
+
 return package
