bc3caddc29cb02ed53a5f58681864724783c44fa
Matt Bierbaum <matt.bierbaum@gmail.com>
2019-05-02 11:23:48 -0400

    adding speedtest to bundle

--- bundle/speedtest.lua
+++ bundle/speedtest.lua
@@ -1,22 +1,23 @@
 local P = require('plinko')
 
 local conf = {
-    dt = 1e-3,
+    dt = 1e-2,
     eps = 1e-6,
     nbl = P.neighborlist.CellNeighborlist(P.objects.Box({0, 0}, {1, 1}), {100, 100}),
     forces = {P.forces.force_gravity},
     particles = {P.objects.SingleParticle({0.501, 0.85}, {0.3, 0}, {0, 0})},
-    objects = {
-        P.objects.Box({0, 0}, {1, 1}),
-        P.objects.Box({.1, .1}, {.2, .2})
-    },
-    observers = {
-        --observers.TimePrinter(1e6)
-    },
+    objects = {P.objects.Box({0, 0}, {1, 1})},
+    observers = {},
 }
 
+local steps = 1e6
 local s = P.ics.create_simulation(conf)
 local t_start = os.clock()
-s:step(1e6)
+s:step(steps)
 local t_end = os.clock()
-print(t_end - t_start)
+local time = t_end - t_start
+local out = string.format(
+    '%0.2f million steps per second (%0.2f seconds)',
+    (steps / time / 1e6), time
+)
+print(out)
