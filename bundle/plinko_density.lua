eb6974a96b931b163ac550567169fac045f63010
Matt Bierbaum <matt.bierbaum@gmail.com>
2019-05-02 14:49:20 -0400

    adding plinko density to bundle

--- bundle/plinko_density.lua
+++ bundle/plinko_density.lua
@@ -1,22 +1,31 @@
 local P = require('plinko')
 
-local O = {3, 10}
-local N = 10000
-local rad = 0.4
-local cargs = {damp=0.8}
+local argparse = require('lib.argparse')
+local opt = argparse(){name='plinko'}
+opt:option('-g', 'Fractional gap between circles', 0.2, tonumber):argname('gap')
+opt:option('-L', 'Lattice dimensions', {3, 8}, P.util.tovec):argname('latt')
+opt:option('-d', 'Collision damping constant', 0.8, tonumber):argname('damp')
+opt:option('-N', 'Number of particles', 10000, tonumber):argname('N')
+opt:argument('filename', 'Filename for output', 'plinko.pgm'):args('?')
+local arg = opt:parse(arg)
+
+local fn = arg.filename or 'plinko.pgm'
+local O = arg.L
+local N = arg.N
+local rad = 0.5 * (1 - arg.g)
+local cargs = {damp=arg.d}
 
 local obj, bd = P.ics.hex_grid_object(O[1], O[2], P.ics.obj_funcs.circle, rad, cargs)
 local w, h = bd[1], bd[2]
+local box = P.objects.Box({0, 0}, bd)
+obj[#obj + 1] = box
 
-local box0 = P.objects.Box({0, 0}, bd)
-local box1 = P.objects.Box({0, 0}, bd)
-local box2 = P.objects.Box({0, 0}, bd)
-obj[#obj + 1] = box0
+P.util.tprint(bd)
 
 local conf = {
     dt = 1e-2,
     eps = 1e-4,
-    nbl = P.neighborlist.CellNeighborlist(box1, {200, 200}, 1e-1),
+    nbl = P.neighborlist.CellNeighborlist(box, {200, 200}, 1e-1),
     forces = {P.forces.force_gravity},
     particles = {
         P.objects.UniformParticles(
@@ -26,11 +35,9 @@ local conf = {
     },
     objects = obj,
     observers = {
-        P.observers.ImageRecorder('./test.img',
-            P.plotting.DensityPlot(box2, 4500/(box2.uu[1] - box2.ll[1]) )
-        ),
+        P.observers.ImageRecorder(fn, P.plotting.DensityPlot(box, 4500/w, 'pgm5')),
         P.observers.TimePrinter(1e4),
-        P.interrupts.Collision(box0.segments[4])
+        P.interrupts.Collision(box.segments[4])
     },
 }
 
