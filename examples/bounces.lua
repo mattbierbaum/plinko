8c2575f338db341a787be3ff5c78343934596083
Matt Bierbaum <matt.bierbaum@gmail.com>
2023-01-29 15:49:35 -0800

    bounces example

--- examples/bounces.lua
+++ examples/bounces.lua
@@ -2,12 +2,12 @@ local P = require('plinko')
 
 local argparse = require('lib.argparse')
 local opt = argparse(){name='plinko'}
-opt:option('-g', 'Fractional gap between circles', '0.2', tonumber):argname('gap')
-opt:option('-L', 'Lattice dimensions %i,%i', '3,8', P.util.tovec):argname('latt')
+opt:option('-g', 'Fractional gap between circles', '0.5', tonumber):argname('gap')
+opt:option('-L', 'Lattice dimensions %i,%i', '2,3', P.util.tovec):argname('latt')
 opt:option('-d', 'Collision damping constant', '0.8', tonumber):argname('damp')
-P.cli.options_particles(opt, {-0.1, -0.1}, {0.1, 0.1}, nil, nil, 'uniform2d')
+P.cli.options_particles(opt, {-0.1, -0.1}, nil, {0.1, 0.1}, nil, 'uniform2d')
 P.cli.options_seed(opt, '10')
-P.cli.options_observer(opt, 'bounces.collisions', '1000')
+P.cli.options_observer(opt, 'bounces.collisions', '4000')
 local arg = opt:parse(arg)
 
 local O = arg.L
@@ -21,18 +21,19 @@ local box = P.objects.Box({0, 0}, bd)
 obj[#obj + 1] = box
 
 local conf = {
-    dt = 1e-3,
+    dt = 1e-2,
     eps = 1e-8,
     nbl = P.neighborlist.CellNeighborlist(box, {200, 200}, 1e-1),
     forces = {P.forces.force_gravity},
-    particles = {P.cli.args_to_particles(arg, {w/2, h-0.5})},
+    particles = {P.cli.args_to_particles(arg, {w/2+0.001, h-0.5})},
     objects = obj,
     observers = {
         P.cli.args_to_observer(arg, box),
-        P.observers.TimePrinter(1e4),
-        P.interrupts.Collision(box.segments[4])
+        P.observers.TimePrinter(1e3),
+        P.interrupts.Collision(box.segments[4]),
+        P.observers.InitialStateRecorder('bounces.p0')
     },
 }
 
 local s = P.ics.create_simulation(conf)
-s:step(100000)
+s:step(10000)
