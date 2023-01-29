a21739fa5be26e9942d0434ef5fa1cc9907668d0
Matt Bierbaum <matt.bierbaum@gmail.com>
2023-01-29 14:26:26 -0800

    bounces example script

---
+++ examples/bounces.lua
@@ -0,0 +1,38 @@
+local P = require('plinko')
+
+local argparse = require('lib.argparse')
+local opt = argparse(){name='plinko'}
+opt:option('-g', 'Fractional gap between circles', '0.2', tonumber):argname('gap')
+opt:option('-L', 'Lattice dimensions %i,%i', '3,8', P.util.tovec):argname('latt')
+opt:option('-d', 'Collision damping constant', '0.8', tonumber):argname('damp')
+P.cli.options_particles(opt, {-0.1, -0.1}, {0.1, 0.1}, nil, nil, 'uniform2d')
+P.cli.options_seed(opt, '10')
+P.cli.options_observer(opt, 'bounces.collisions', '1000')
+local arg = opt:parse(arg)
+
+local O = arg.L
+local N = arg.Nv
+local rad = 0.5 * (1 - arg.g)
+local cargs = {damp=arg.d}
+
+local obj, bd = P.ics.hex_grid_object(O[1], O[2], P.ics.obj_funcs.circle, rad, cargs)
+local w, h = bd[1], bd[2]
+local box = P.objects.Box({0, 0}, bd)
+obj[#obj + 1] = box
+
+local conf = {
+    dt = 1e-3,
+    eps = 1e-8,
+    nbl = P.neighborlist.CellNeighborlist(box, {200, 200}, 1e-1),
+    forces = {P.forces.force_gravity},
+    particles = {P.cli.args_to_particles(arg, {w/2, h-0.5})},
+    objects = obj,
+    observers = {
+        P.cli.args_to_observer(arg, box),
+        P.observers.TimePrinter(1e4),
+        P.interrupts.Collision(box.segments[4])
+    },
+}
+
+local s = P.ics.create_simulation(conf)
+s:step(100000)
