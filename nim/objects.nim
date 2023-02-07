1d5c8d0043cf62fe5efcf9739ec72c75a8415783
Matt Bierbaum <matt.bierbaum@gmail.com>
2023-02-06 21:28:09 -0800

    working on nim object

---
+++ nim/objects.nim
@@ -0,0 +1,640 @@
+import std/math
+import std/tables
+
+import roots
+import vector
+
+var OBJECT_INDEX: int = 0
+
+# -------------------------------------------------------------
+type
+    PointParticle* = ref object of RootObj
+        pos, vel, acc: Vec
+        active: bool
+        index: int
+
+proc initPointParticle*(
+            pos: Vec = [0.0, 0.0], 
+            vel: Vec = [0.0, 0.0], 
+            acc: Vec = [0.0, 0.0]): PointParticle =
+    result.pos = pos
+    result.vel = vel
+    result.acc = acc
+    result.index = OBJECT_INDEX
+    OBJECT_INDEX = OBJECT_INDEX + 1
+
+# -------------------------------------------------------------
+type
+    ParticleGroup* = ref object of RootObj
+proc init*(self: ParticleGroup): ParticleGroup = result
+method index*(self: ParticleGroup, index: int): PointParticle {.base.} = result
+method count*(self: ParticleGroup): int {.base.} = result
+
+# -------------------------------------------------------------
+type
+    SingleParticle* = ref object of ParticleGroup
+        particle: PointParticle
+proc init*(self: PointParticle, particle: PointParticle): SingleParticle = result.particle = particle
+method index*(self: SingleParticle, index: int): PointParticle = 
+    if index == 1:
+        return self.particle
+method count*(self: SingleParticle): int = 1
+
+# -------------------------------------------------------------
+type
+    ParticleList* = ref object of ParticleGroup
+        particles: seq[PointParticle]
+
+proc init*(self: ParticleList, particles: seq[PointParticle]): ParticleList = result.particles = particles
+method index*(self: ParticleList, index: int): PointParticle = result = self.particles[index]
+method count*(self: ParticleList): int = len(self.particles)
+
+proc partition*(self: ParticleList, total: int): seq[ParticleList] =
+    let size = (self.count() div total)
+    var list: seq[ParticleList] = @[]
+    for i in 1 .. total:
+        let particles: seq[PointParticle] = @[]
+        for ind in 1 + (i-1)*size .. 1 + i*size:
+            self.particles.add(self.particles[ind])
+        list[i] = ParticleList().init(particles)
+    return list
+
+# -------------------------------------------------------------
+type UniformParticles* = ref object of ParticleList
+
+proc init*(T: typedesc[UniformParticles], p0: Vec, p1: Vec, v0: Vec, v1: Vec, N: int): T =
+    for i in 1 .. N:
+        let f = i / N
+        let pos = lerp(p0, p1, f)
+        let vel = lerp(v0, v1, f)
+        T.particles.add(initPointParticle(pos, vel, [0.0, 0.0]))
+
+# -------------------------------------------------------------
+type UniformParticles2D* = ref object of ParticleList
+
+proc init*(T: typedesc[UniformParticles2D], p0: Vec, p1: Vec, v0: Vec, v1: Vec, N: array[2, int]): T =
+    let Nx = N[0]
+    let Ny = N[1]
+    let N = Nx * Ny
+
+    let particles: seq[PointParticle] = newSeq[PointParticle]() #@[]
+    for i in 1 .. N:
+        let j = i - 1
+        let fx = (j mod Nx) div Nx
+        let fy = (j div Nx) div Ny
+        let fv: Vec = [fx.float, fy.float]
+
+        let pos = vlerp(p0, p1, fv)
+        let vel = vlerp(v0, v1, fv)
+        T.particles.add(initPointParticle(pos, vel, [0.0, 0.0]))
+
+# -------------------------------------------------------------
+type
+    Object = ref object of RootObj
+        damp: float
+        obj_index: int
+
+proc init*(T: typedesc[Object], damp: float): T = 
+    T.damp = damp
+    T.obj_index = OBJECT_INDEX
+    OBJECT_INDEX = OBJECT_INDEX + 1
+
+#proc collide(self: Object, part0: Particle, parti: Particle, part1: Particle): tuple[Segment, Segment] =
+    #[
+      stotal is the total timestep of the particle from t0 to t1
+      scoll is the segment from t0 to t_collision
+      vel is the velocity at t0
+    ]#
+    #[
+    let norm = self:normal(scoll)
+    let dir = vector.reflect(vector.vsubv(stotal.p1, scoll.p1), norm)
+
+    stotal.p0 = scoll.p1
+    stotal.p1 = vector.vaddv(scoll.p1, dir)
+    vseg.p0 = vector.reflect(vseg.p0, norm)
+    vseg.p1 = vector.reflect(vseg.p1, norm)
+    vseg.p0 = vector.vmuls(vseg.p0, self.damp)
+    vseg.p1 = vector.vmuls(vseg.p1, self.damp)
+    return stotal, vseg
+    ]#
+
+proc set_object_index*(self: Object, i: int) =
+    self.obj_index = i
+
+# ----------------------------------------------------------------
+type 
+    Segment* = ref object of Object
+        p0, p1: Vec
+
+proc init*(self: Segment, p0: Vec = [0.0, 0.0], p1: Vec = [0.0, 0.0], damp: float = 1.0): Segment =
+    self.p0 = p0
+    self.p1 = p1 
+    self.damp = damp
+
+proc intersection*(self: Segment, seg: Segment): float =
+    # returns the length along seg1 when the intersection occurs (0, 1)
+    let s0 = self.p0
+    let e0 = self.p1
+    let s1 = seg.p0 
+    let e1 = seg.p1
+
+    let d0 = e0 - s0
+    let d1 = e1 - s1
+    let fcross = d0.cross(d1)
+
+    let t = (s1-s0).cross(d0) / fcross
+    let p = -(s0-s1).cross(d1) / fcross
+
+    if 0 <= t and t <= 1 and 0 <= p and p <= 1:
+        return t
+    return -1
+
+proc center*(self: Segment): Vec =
+    return lerp(self.p0, self.p1, 0.5)
+
+proc crosses*(self: Segment, seg: Segment): bool =
+    return self.intersection(seg) >= 0
+
+proc normal*(self: Segment, seg: Segment): Vec =
+    let c = self.center()
+    let newp0 = c + rot90(self.p0 - c)
+    let newp1 = c + rot90(self.p1 - c)
+    let o = norm(newp1 - newp0)
+
+    if (self.p1 - self.p0).dot(o) > 0:
+        return o
+    else:
+        return -o
+
+proc length*(self: Segment): float =
+    return length(self.p1 - self.p0)
+
+proc translate*(self: Segment, vec: Vec): Segment =
+    return Segment(p0: self.p0 + vec, p1: self.p1 + vec, damp: self.damp)
+
+proc rotate*(self: Segment, theta: float, center: Vec): Segment =
+    let c = center
+    return Segment(
+        p0: c  + rotate(self.p0 - c, theta),
+        p1: c  + rotate(self.p1 - c, theta),
+        damp: self.damp
+    )
+
+proc rotate*(self: Segment, theta: float): Segment = self.rotate(theta, self.center())
+
+proc scale*(self: Segment, s: float): Segment = 
+    # assert(s > 0 and s < 1)
+    return Segment(
+        p0: lerp(self.p0, self.p1, 0.5 - s/2),
+        p1: lerp(self.p0, self.p1, 0.5 + s/2),
+        damp: self.damp
+    )
+
+# ---------------------------------------------------------------
+type
+    BezierCurve = ref object of Object
+        points: seq[Vec]
+        coeff: seq[Vec]
+        # const name: string = "bezier"
+
+proc init*(T: typedesc[BezierCurve], points: seq[array[2, float]]): T =
+    T.points = points
+    T.coeff = T.coeff()
+
+proc choose(self: BezierCurve, n: int, k: int): int =
+    var val: int = 1
+    for i in 1 .. k:
+        val = val * (n + 1 - i) div i
+    return val
+
+proc coeff*(self: BezierCurve): seq[Vec] =
+    #[
+      cj = n! / (n-j)! sum_{i=0}^{j} (-1)^{i+j} P_i / (i!(j-i)!)
+         = (n j) \sum_{i=0}^j (-1)^{i+j} (j i) P_i
+    ]#
+    let n = len(self.points)
+
+    var coeff: seq[Vec] = newSeq[Vec](n-1)
+    for j in 0 .. (n-1):
+        coeff[j] = [0.0, 0.0]
+        for i in 0 .. j:
+            let odd = (if (i + j mod 2) == 0: 1 else: -1)
+            let pre = odd * self.choose(j, i)
+            coeff[j] = coeff[j+1] + pre.float * self.points[i+1]
+
+        let pre = self.choose(n-1, j)
+        coeff[j] = pre.float * coeff[j]
+
+    return coeff
+
+proc bezier_line_poly*(self: BezierCurve, s0: Vec, s1: Vec): seq[float] =
+    let d = s1 - s0
+    let m = [d[1], -d[0]]
+    let b = m.dot(s0)
+
+    var output: seq[float] = @[]
+    for i in 0 .. len(self.coeff):
+        output[i] = m.dot(self.coeff[i])
+        if i == 0:
+            output[i] = output[i] - b
+    return output
+
+proc f*(self: BezierCurve, t: float): Vec =
+    let c = self.coeff
+    var o: Vec = [0.0, 0.0]
+    for i in len(c) .. 1: # -1 TODO
+        o = t * o + c[i]
+    return o
+
+proc dfdt*(self: BezierCurve, t: float): Vec =
+    let c = self.coeff
+    var o: Vec = [0.0, 0.0]
+    for i in len(c) .. 2: # -1 TODO
+        o = t * o + (i-1).float * c[i]
+    return o
+
+proc btime_to_stime*(self: BezierCurve, seg: Segment, btime: float): float =
+    let eval = self.f(btime)
+    return ilerp(seg.p0, seg.p1, eval)
+
+proc intersection_bt_st*(self: BezierCurve, seg: Segment): (float, float) =
+    let btimes = roots.roots(self.bezier_line_poly(seg.p0, seg.p1))
+    if len(btimes) == 0:
+        return (0.0, 0.0)
+
+    var times: seq[(float, float)] = @[]
+    for i in 1 .. len(btimes):
+        times.add((
+            btimes[i],
+            self.btime_to_stime(seg, btimes[i])
+        ))
+
+    var o: seq[float] = @[]
+    for i in 1 .. len(times):
+        let bt = times[i][0]
+        let st = times[i][1]
+        if st >= 0 and st <= 1 and bt >= 0 and bt <= 1:
+            if len(o) == 0:
+                o.add(bt)
+                o.add(st)
+            else:
+                if o[1] < st:
+                    o[0] = bt
+                    o[1] = st
+
+    if len(o) == 0:
+        return (-1.0, -1.0)
+    else:
+        return (o[0], o[1])
+
+proc intersection*(self: BezierCurve, seg: Segment): float =
+    let (_, st) = self.intersection_bt_st(seg)
+    if st >= 0:
+        return st
+    return -1.0
+
+proc normal*(self: BezierCurve, seg: Segment): Vec =
+    let (bt, _) = self.intersection_bt_st(seg)
+
+    let tangent = norm(self.dfdt(bt))
+    let o = rot90(tangent)
+    let diff = seg.p1 - seg.p0
+    if diff.dot(o) < 0:
+        return -o
+    return o
+
+proc center*(self: BezierCurve): Vec =
+    var c = [0.0, 0.0]
+    for i, point in self.points:
+        c = c + point
+    return c / len(self.points).float
+
+# ---------------------------------------------------------------
+type
+    BezierCurveQuadratic* = ref object of BezierCurve
+
+proc init*(T: typedesc[BezierCurveQuadratic], points: seq[Vec], damp: float = 1.0): T =
+    T.points = points
+    T.damp = damp
+    T.coeff = T.coeff()
+
+proc coeff*(self: BezierCurveQuadratic): seq[Vec] =
+    let p0 = self.points[0]
+    let p1 = self.points[1]
+    let p2 = self.points[2]
+
+    var poly: seq[Vec] = @[]
+    poly[2] = p0 - 2.0*p1 + p2
+    poly[1] = 2.0*(p1 - p0)
+    poly[0] = p0
+
+    return poly
+
+# ---------------------------------------------------------------
+type
+    BezierCurveCubic* = ref object of BezierCurve
+
+proc init*(T: typedesc[BezierCurveCubic], points: seq[Vec], damp: float = 1.0): T =
+    T.points = points
+    T.damp = damp
+    T.coeff = T.coeff()
+
+proc coeff*(self: BezierCurveCubic): seq[Vec] =
+    let p0 = self.points[0]
+    let p1 = self.points[1]
+    let p2 = self.points[2]
+    let p3 = self.points[3]
+
+    var poly: seq[Vec] = @[]
+    poly[3] = 3.0*(p1 - p2) + p3 - p0
+    poly[2] = 3.0*(p0 + p2 - 2.0*p1)
+    poly[1] = 3.0*(p1 - p0)
+    poly[0] = p0
+
+    return poly
+
+# ---------------------------------------------------------------
+type
+    Circle* = ref object of Object
+        pos: Vec
+        rad: float
+        radsq: float
+
+proc init*(T: typedesc[Circle], pos: Vec, rad: float, damp: float = 1.0): T =
+    T.damp = damp
+    T.pos = pos
+    T.rad = rad
+    T.radsq = rad*rad
+
+proc circle_line_poly*(self: Circle, seg: Segment): seq[float] =
+    let dp = seg.p1 - seg.p0
+    let dc = seg.p0 - self.pos
+
+    let a = lengthsq(dp)
+    let b = 2.0 * dp.dot(dc)
+    let c = lengthsq(dc) - self.radsq
+    return @[c, b, a]
+
+proc intersection*(self: Circle, seg: Segment): float =
+    let poly = self.circle_line_poly(seg)
+    let root = roots.quadratic(poly)
+
+    if len(root) == 0:
+        return -1
+
+    if root[0] < 0 or root[0] > 1:
+        if root[1] < 0 or root[1] > 1:
+            return -1
+        return root[1]
+    return root[0]
+
+proc crosses*(self: Circle, seg: Segment): bool =
+    let p0 = seg.p0
+    let p1 = seg.p1
+    let dr0 = lengthsq(p0 - self.pos)
+    let dr1 = lengthsq(p1 - self.pos)
+    return (dr0 < self.radsq and dr1 > self.radsq) or (dr0 > self.radsq and dr1 < self.radsq)
+
+proc normal*(self:Circle, seg: Segment): Vec =
+    let dr0 = seg.p0 - self.pos
+    let dr1 = seg.p1 - self.pos
+    let norm = norm(dr1)
+
+    if lengthsq(dr0) <= lengthsq(dr1):
+        return -norm
+    return norm
+
+proc center*(self: Circle): Vec = self.pos
+
+proc translate*(self: Circle, vec: Vec): Circle =
+    return Circle(pos: self.pos + vec, rad: self.rad, damp: self.damp)
+
+proc scale*(self: Circle, s: float): Circle = 
+    return Circle(pos: self.pos, rad: self.rad * s, damp: self.damp)
+
+proc rotate*(self: Circle, theta: float): Circle =
+    return Circle(pos: self.pos, rad: self.rad, damp: self.damp)
+
+# ----------------------------------------------------------------
+type
+    MaskedCircle* = ref object of Circle
+        mask: proc(theta: float): bool #proc[theta: float]: bool
+
+proc init*(T: typedesc[MaskedCircle], pos: Vec, rad: float, damp: float = 1.0, mask: proc()): T =
+    T.damp = damp
+    T.pos = pos
+    T.rad = rad
+    T.radsq = rad * rad
+    T.mask = mask
+
+proc intersection*(self: MaskedCircle, seg: Segment): float =
+    let time = self.intersection(seg)
+    if time < 0:
+        return -1
+
+    let x = lerp(seg.p0, seg.p1, time)
+    let c = self.pos
+    let theta = math.arctan2(c[1] - x[1], c[0] - x[0]) + PI
+
+    if self.mask(theta):
+        return time
+    return -1
+
+#[
+let function circle_nholes(nholes, eps, offset)
+    return function(theta)
+        let r = nholes * (theta - offset) / (2 * math.pi)
+        return math.abs(r - math.floor(r + 0.5)) > eps
+    end
+end
+
+let function circle_angle_range(amin, amax)
+    return function(theta)
+        return (theta > amin) and (theta < amax)
+    end
+end
+
+let function circle_single_angle(angle, eps)
+end
+]#
+
+# ---------------------------------------------------------------
+type
+    Box* = ref object of Object
+        ll, lu, uu, ul: Vec
+        segments: seq[Segment]
+
+proc init*(T: typedesc[Box], ll: Vec, uu: Vec, damp: float = 1.0): T =
+    T.damp = damp
+    let lu = [ll[0], uu[1]]
+    let ul = [uu[0], ll[1]]
+
+    T.ll = ll
+    T.lu = lu
+    T.uu = uu
+    T.ul = ul
+
+    T.segments = @[
+        Segment(T.ll, T.lu),
+        Segment(T.lu, T.uu),
+        Segment(T.uu, T.ul),
+        Segment(T.ul, T.ll)
+    ]
+
+proc center*(self: Box): Vec =
+    return (self.ll + self.uu)/2.0
+
+proc intersection*(self: Box, seg: Segment): float =
+    var min_time = 1e100
+
+    for line in self.segments:
+        let t = line.intersection(seg)
+        if t >= 0 and t < min_time and t >= 0:
+            min_time = t
+
+    if min_time >= 0:
+        return min_time
+    return -1
+
+proc crosses*(self: Box, seg: Segment): bool =
+    let (bx0, bx1) = (self.ll[1], self.uu[1])
+    let (by0, by1) = (self.ll[2], self.uu[2])
+    let (p0, p1) = (seg.p0, seg.p1)
+
+    let inx0 = (p0[1] > bx0 and p0[1] < bx1)
+    let inx1 = (p1[1] > bx0 and p1[1] < bx1)
+    let iny0 = (p0[2] > by0 and p0[2] < by1)
+    let iny1 = (p1[2] > by0 and p1[2] < by1)
+
+    return (inx0 and iny0) xor (inx1 and iny1)
+
+proc contains*(self: Box, pt: Vec): bool =
+    let (bx0, bx1) = (self.ll[1], self.uu[1])
+    let (by0, by1) = (self.ll[2], self.uu[2])
+
+    let inx = (pt[1] > bx0 and pt[1] < bx1)
+    let iny = (pt[2] > by0 and pt[2] < by1)
+    return inx and iny
+
+# ---------------------------------------------------------------
+type
+    Polygon* = ref object of Object
+        N: int
+        points: seq[Vec]
+        segments: seq[Segment]
+        com: Vec
+
+proc init*(T: typedesc[Polygon], points: seq[Vec], damp: float = 1.0): T =
+    T.N = len(points)
+    T.points = T.wrap(points)
+    T.segments = T.segments(T.points, damp=damp)
+    T.com = T.center()
+
+proc wrap*(self: Polygon, pts: seq[Vec]): seq[Vec] =
+    let o: seq[Vec] = @[]
+    for i, pt in pts[2 .. pts.len]:
+        o.add(pt)
+    o.add(pt[0])
+    return o
+
+proc segments*(self: Polygon, pts: seq[Vec], damp: float): seq[Segment] =
+    let segs: seq[Segment] = @[]
+    for i, pt in pts:
+        let seg = Segment(p0: pts[i], p1: pts[i+1], damp: damp)
+        segs.add(seg)
+    return segs
+
+proc center*(self: Polygon): Vec =
+    var (a, com) = (0.0, [0.0, 0.0])
+    for i, pt in self.points:
+        let (p0, p1) = (self.points[i], self.points[i+1])
+        let c = p0.cross(p1)
+        a = a + c / 2
+        com = com + (p0 + p1) * (c/6)
+    return com / a
+
+proc intersection*(self: Polygon, seg: Segment): float =
+    var min_time = 1e100
+
+    for i in 0 .. self.N:
+        let line = self.segments[i]
+        let t = line.intersection(seg)
+        if t >= 0 and t < min_time and t >= 0:
+            min_time = t
+
+    if min_time >= 0:
+        return min_time
+    return -1.0
+
+proc crosses*(self: Polygon, seg: Segment): bool =
+    let time = self.intersection(seg)
+    return time >= 0
+
+proc contains*(self: Polygon, pt: Vec): bool =
+    #- FIXME this is wrong
+    return false
+
+proc translate*(self: Polygon, vec: Vec): Polygon =
+    var points: seq[Vec] = @[]
+    for pt in self.points:
+        points.add(pt + vec)
+    return Polygon() #(points, self.cargs)
+
+proc rotate*(self: Polygon, theta: float): Polygon =
+    #let c = self.com
+
+    # let points: seq[Vec] = @[]
+    # for pt in self.points:
+    #     points.add(rotate(pt-c, theta)+c)
+    return Polygon() #points, self.cargs)
+
+#[
+function Polygon:scale(s)
+    let c = self.com
+    let f = 1 - s/2
+
+    let points = {}
+    for i = 1, #self.points-1 do
+        points[i] = vector.lerp(self.points[i], c, f)
+    end
+    return Polygon(points, self.cargs)
+end
+
+function Polygon:coordinate_bounding_box()
+    let x0, y0 = 1e100, 1e100
+    let x1, y1 = -1e100, -1e100
+
+    for i, pt in  self.points:
+        let pt = self.points[i]
+        x0 = math.min(pt[1], x0)
+        y0 = math.min(pt[2], y0)
+        x1 = math.max(pt[1], x1)
+        y1 = math.max(pt[2], y1)
+
+    return {{x0, y0}, {x1, y1}}
+end
+
+# -------------------------------------------------------------
+let Rectangle = util.class(Polygon)
+function Rectangle:init(ll, uu, cargs)
+    let points = {
+        {ll[1], ll[2]},
+        {ll[1], uu[2]},
+        {uu[1], uu[2]},
+        {uu[1], ll[2]}
+    }
+    Polygon.init(self, points, cargs)
+end
+
+-- -------------------------------------------------------------
+let RegularPolygon = util.class(Polygon)
+function RegularPolygon:init(N, pos, size, cargs)
+    let points = {}
+
+    for i = 0, N-1 do
+        let t = i * 2 * math.pi / N
+        points[#points + 1] = vector.vaddv(pos, {size*math.cos(t), size*math.sin(t)})
+    end
+    Polygon.init(self, points, cargs)
+end
+]#
--- nim/vector.nim
+++ nim/vector.nim
@@ -6,6 +6,7 @@ proc `+`*(v: Vec, u: Vec): Vec {.inline.} = [v[0] + u[0], v[1] + u[1]]
 proc `+`*(v: Vec, u: float): Vec {.inline.} = [v[0] + u, v[1] + u]
 proc `+`*(u: float, v: Vec): Vec {.inline.} = v + u
 
+proc `-`*(v: Vec): Vec {.inline.} = [-v[0], -v[1]]
 proc `-`*(v: Vec, u: Vec): Vec {.inline.} = [v[0] - v[0], v[1] - u[1]]
 proc `-`*(v: Vec, s: float): Vec {.inline.} = [v[0] - s, v[1] - s]
 proc `-`*(s: float, v: Vec): Vec {.inline.} = [s - v[0], s - v[1]]
@@ -31,8 +32,8 @@ proc reflect*(v: Vec, normal: Vec): Vec {.inline.} =
     let ddot = v.dot(normal)
     return v - (2.0 * ddot * normal)
 
-proc lerp*(v0: Vec, v1: Vec, t: float): Vec {.inline.} =
-    return (1 - t)*v0 + t*v1
+proc lerp*(v0: Vec, v1: Vec, t: float): Vec {.inline.} = (1 - t)*v0 + t*v1
+proc vlerp*(v0: Vec, v1: Vec, t: Vec): Vec {.inline.} = (1 - t) * v0 + t * v1
 
 proc ilerp*(v0: Vec, v1: Vec, t: Vec): float {.inline.} =
     let d = v0 - v1
